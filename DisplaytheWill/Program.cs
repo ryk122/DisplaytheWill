2e914445dba134290fd902b91a5e6a8de5db379d
ryk122 <54937624+ryk122@users.noreply.github.com>
2020-06-18 16:47:19 +0900

    first commit

---
+++ DisplaytheWill/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+
+namespace DisplaytheWill
+{
+    static class StartClass
+    {
+        static void Main(string[] args)
+        {
+            MainClass mc = new MainClass();
+            Console.ReadKey();
+        }
+    }
+
+    class MainClass
+    {
+        public MainClass()
+        {
+            Console.WriteLine("new class");
+
+            StreamWriter writer = new StreamWriter("E:\\KBTest.txt");
+            writer.WriteLine("File created using StreamWriter class.");
+            writer.Close();
+        }
+    }
+}
